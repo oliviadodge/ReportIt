58b2e3ea9b253516c618d90f6e5e16cee491d805
Olivia <oliviadodge@gmail.com>
2020-11-04 23:29:26 -0800

    Change DashboardFragment.kt to MapFragment.kt

--- app/src/main/java/com/populus/reportit/ui/map/MapFragment.kt
+++ app/src/main/java/com/populus/reportit/ui/map/MapFragment.kt
@@ -1,4 +1,4 @@
-package com.populus.reportit.ui.dashboard
+package com.populus.reportit.ui.map
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.populus.reportit.R
 
-class DashboardFragment : Fragment() {
+class MapFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var mapViewModel: MapViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        mapViewModel =
+                ViewModelProvider(this).get(MapViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        mapViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
