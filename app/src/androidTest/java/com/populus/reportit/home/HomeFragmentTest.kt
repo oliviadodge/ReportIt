ce340432ff0b93fb935c52777291d874fa30459b
Olivia <oliviadodge@gmail.com>
2020-11-21 00:10:54 -0800

    Debug issue with "no tests found"

--- app/src/androidTest/java/com/populus/reportit/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/populus/reportit/home/HomeFragmentTest.kt
@@ -1,16 +1,6 @@
 package com.populus.reportit.home
 
-import android.os.Bundle
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.populus.reportit.MainActivityPage
-import com.populus.reportit.R
-import com.populus.reportit.ui.home.HomeFragment
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,12 +8,12 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
 
-    private lateinit var scenario: FragmentScenario<HomeFragment>
+//    private lateinit var scenario: FragmentScenario<HomeFragment>
     private lateinit var page: HomeFragmentPage
 
     @Before
     fun setupPage() {
-        scenario = launchFragmentInContainer<HomeFragment>()
+//        scenario = launchFragmentInContainer<HomeFragment>()
         page = HomeFragmentPage()
     }
 
