1e2cd1cb9a6576935f29a8a77dc214bc7fd6a1ba
Olivia <oliviadodge@gmail.com>
2020-11-23 00:11:57 -0800

    Add back the fragment scenario dependency

--- app/src/androidTest/java/com/populus/reportit/MainActivityTest.kt
+++ app/src/androidTest/java/com/populus/reportit/MainActivityTest.kt
@@ -2,27 +2,21 @@ package com.populus.reportit
 
 
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Rule
-    @JvmField
-    var mActivityTestRule: ActivityScenario<MainActivity> = ActivityScenario.launch(MainActivity::class.java)
-    
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+
     private lateinit var page: MainActivityPage
 
     @Before
     fun setupPage() {
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
         page = MainActivityPage()
     }
 
--- app/src/androidTest/java/com/populus/reportit/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/populus/reportit/home/HomeFragmentTest.kt
@@ -1,6 +1,9 @@
 package com.populus.reportit.home
 
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.populus.reportit.ui.home.HomeFragment
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -8,12 +11,12 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
 
-//    private lateinit var scenario: FragmentScenario<HomeFragment>
+    private lateinit var scenario: FragmentScenario<HomeFragment>
     private lateinit var page: HomeFragmentPage
 
     @Before
     fun setupPage() {
-//        scenario = launchFragmentInContainer<HomeFragment>()
+        scenario = launchFragmentInContainer<HomeFragment>()
         page = HomeFragmentPage()
     }
 
